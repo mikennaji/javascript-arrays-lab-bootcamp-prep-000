457b4fad896695941cca87cfb20c3a621f581f43
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-24 22:06:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+}
+function appendKitten(name){
+  var newkittens = [...kittens]
+  newkittens.push(name)
+  return(newkittens)
+}
+function prependKitten(name){
+  var newkittens = [...kittens]
+  newkittens.unshift(name)
+  return(newkittens)
+}
+function removeLastKitten(name){
+  var newkittens = [...kittens]
+  newkittens.pop(name)
+  return(newkittens)
+}
+function removeFirstKitten(name){
+  var newkittens = [...kittens]
+  newkittens.shift(name)
+  return(newkittens)
+}
